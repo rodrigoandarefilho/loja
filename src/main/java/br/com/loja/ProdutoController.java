bdf537373c1a297ba26cbf5a7591d1c46c87dd09
rodrigo andare filho <rodrigoandare.f@gmail.com>
2023-08-23 18:28:59 -0300

    feat: Buscar Produto Por Id

--- src/main/java/br/com/loja/ProdutoController.java
+++ src/main/java/br/com/loja/ProdutoController.java
@@ -3,11 +3,10 @@ package br.com.loja;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/produtos")
@@ -23,5 +22,11 @@ public class ProdutoController {
         produtoRepository.save(produto);
         var uri = uriBuilder.path("/produtos/{id}").buildAndExpand(produto.getId()).toUri();
         return ResponseEntity.created(uri).body(new Produto(produto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity buscarProdutoPorId(@PathVariable BigDecimal id) {
+        var produto = produtoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new Produto(produto));
     }
 }
